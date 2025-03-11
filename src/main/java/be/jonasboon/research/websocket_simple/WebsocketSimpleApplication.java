c425fb7a763e072df2237f939869786a76827332
JonasBoonA <jonas.boon@acerta.be>
2025-03-11 18:09:04 +0100

    Initialise project

---
+++ src/main/java/be/jonasboon/research/websocket_simple/WebsocketSimpleApplication.java
@@ -0,0 +1,13 @@
+package be.jonasboon.research.websocket_simple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketSimpleApplication.class, args);
+	}
+
+}
